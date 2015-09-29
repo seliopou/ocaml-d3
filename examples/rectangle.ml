0cbcb748acd7413b200e46fdab5290e76986b515
Spiros Eliopoulos <seliopou@gmail.com>
2015-09-29 17:45:15 -0400

    react: setup rect example for reactivity

--- examples/rectangle.ml
+++ examples/rectangle.ml
@@ -1,24 +1,42 @@
 open D3
 
-type dims = 
-  { width : int; height : int }
+type layout =
+  { width : int; height : int; padding : int }
 
-let view dims padding =
+let view =
+  let s = string_of_int in
+  (* Note that the view no longer takes any arguments for dimensions or
+   * padding. Rather, those parameters are passed via the datum that's bound to
+   * the parent context. The [svg] operation access the height and width by
+   * using a function as an argument to [attr]. That way, if the width or heigh
+   * change, the attribute will be updated. Similarly for the width, height, x,
+   * and y attributes for [rect]. 
+   *
+   * Also, note that the [append] operation has been replaced with [static].
+   * This ensures that on redraws, no additional elements will be appended. *)
   let svg =
-    append "svg"
-    |. int attr "width"  dims.width
-    |. int attr "height" dims.height
+    static "svg"
+    |. attr "width"  (fun _ d _ -> s d.width)
+    |. attr "height" (fun _ d _ -> s d.height)
   in
   let rect =
-    append "rect"
+    static "rect"
     |. str attr "fill"   "black"
-    |. int attr "width"  (dims.width - 2 * padding)
-    |. int attr "height" (dims.height - 2 * padding)
-    |. int attr "x"      padding
-    |. int attr "y"      padding
+    |. attr "width"  (fun _ d _ -> s (d.width  - 2 * d.padding))
+    |. attr "height" (fun _ d _ -> s (d.height - 2 * d.padding))
+    |. attr "x" (fun _ d _ -> s d.padding)
+    |. attr "y" (fun _ d _ -> s d.padding)
   in
   svg <.> rect
 ;;
 
+let layout =
+  { width = 300; height = 300; padding = 20 }
+
+let go layout =
+  run "body" layout view
+
+(* You can make the view reactive to changes in layout by mapping the [go]
+ * function over a [layout React.signal] *)
 let _ =
-  run "body" () (view { width = 300; height = 300 } 20)
+  go layout
